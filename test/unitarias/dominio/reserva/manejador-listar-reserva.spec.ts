ac420920a4acd1193f52cbff616f8959f8254a44
Naren Felipe Parra Trujillo - Ceiba Software <naren.parra@ceiba.com.co>
2020-11-04 12:34:32 -0500

    prueba unitaria manejador listar reserva Mysql

--- test/unitarias/dominio/reserva/manejador-listar-reserva.spec.ts
+++ test/unitarias/dominio/reserva/manejador-listar-reserva.spec.ts
@@ -1,6 +1,7 @@
 import { SinonStubbedInstance } from 'sinon';
 import { ManejadorListarReserva } from 'src/aplicacion/reserva/consulta/listar-reserva.manejador';
 import { DaoReserva } from 'src/dominio/reserva/puerto/dao/dao-reserva';
+import { DaoReservaMysql } from 'src/infraestructura/reserva/adaptador/dao/dao-reserva-mysql';
 import { createStubObj } from 'test/util/create-object.stub';
 
 describe('manejadorListarReserva', () => {
@@ -16,5 +17,21 @@ describe('manejadorListarReserva', () => {
     await manejadorListarReserva.ejecutar();
 
     expect(DaoReservaStub.listar.getCalls().length).toBe(1);
+  });
+});
+
+describe('manejadorListarReserva', () => {
+  let DaoReservaMysqlStub: SinonStubbedInstance<DaoReservaMysql>;
+  let manejadorListarReserva: ManejadorListarReserva;
+
+  beforeEach(() => {
+    DaoReservaMysqlStub = createStubObj<DaoReserva>(['listar']);
+    manejadorListarReserva = new ManejadorListarReserva(DaoReservaMysqlStub);
+  });
+
+  it('listar las reservas ', async () => {
+    await manejadorListarReserva.ejecutar();
+
+    expect(DaoReservaMysqlStub.listar.getCalls().length).toBe(1);
   });
 });
