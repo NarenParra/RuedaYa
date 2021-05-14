76431ad58b77586eb94682ee4f34301ec929d3e1
Naren Parra <naren21p@gmail.com>
2021-05-14 13:08:24 -0500

    config sonar

--- sonar-project.js
+++ sonar-project.js
@@ -2,16 +2,16 @@ const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner(
   {
-    serverUrl: process.env.SONAR_SERVER_URL,
+    serverUrl: "http://localhost:9000",
     options: {
-      'sonar.projectKey': process.env.SONAR_PROJECT_KEY,
+      'sonar.projectKey': "prueba",
       'sonar.projectVersion': '1.0.0',
       'sonar.sources': 'src',
       'sonar.tests': 'src',
       'sonar.test.inclusions': '**/*.test.ts,**/*.test.tsx',
       'sonar.exlusions':'src/tests',
       'sonar.sourceEncoding': 'UTF-8',
-      'sonar.login': process.env.SONAR_LOGIN,
+      'sonar.login': "01935bbc9344f8a59201624046000db6285f8af1",
       'sonar.javascript.file.suffixes': '.tsx,.ts',
       'sonar.lang.patterns.ts': '*/.ts,*/.tsx',
       'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
